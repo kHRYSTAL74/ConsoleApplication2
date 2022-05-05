ecf502badd9e009a99c9fd5618b0458f44732ef9
21201010 <21201010@I301W07>
2022-05-05 11:32:37 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+﻿#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+bool IsPointInCircle(double R, double x, double y);
+
+int main()
+{
+    int Npoints = (int)1E3;
+    int Nincircle = 0;
+    srand(time(0));
+    for (int i = 0; i < Npoints; i++)
+    {
+        if
+            (
+                IsPointInCircle
+                (
+                    1.0,
+                    (rand() % (4 * Npoints) - 2 * Npoints) / (2.0 * Npoints),
+                    (rand() % (4 * Npoints) - 2 * Npoints) / (2.0 * Npoints)
+                )
+                )
+            Nincircle++;
+    }
+    cout << "Calc  Value of PI : " << setprecision(5) << Nincircle * 4.0 / Npoints << endl;
+    cout << "Check Value of PI : " << setprecision(5) << asin(1.0) * 2.0 << endl;
+    cin.get();
+    return 0;
+}
+
+bool IsPointInCircle(double R, double x, double y)
+{
+    return ((x * x + y * y) < R * R);
+}
